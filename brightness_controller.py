f62eb830ab508d79cbb36650417c13654992611b
Mukesh Tiwari <mukeshtiwari1987@gmail.com>
2016-09-24 03:51:26 +0530

    Mukesh added while loop with Pulkit

--- brightness_controller.py
+++ brightness_controller.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import sys
+import time
 brightness_value = 0.5
 keylogger_process = 0
 
@@ -21,16 +22,26 @@ def set_brightness( brightness_value ):
   cmd = './lib/brightness -m ' + str(brightness_value)
   os.system(cmd)
 
-keylogger_process = start_keylogger()
-
 # clear_logger()
 # keylogger_process.kill()
 
 def get_logger_lines_count():
   num_lines = sum(1 for line in open('keystroke.log'))
-  print num_lines
+  return num_lines
 
 def get_logger_last_line():
   args = ("tail", "-n", "1", "keystroke.log")
   popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-  print popen.stdout.read()
+  popen.wait()
+  return popen.stdout.read()
+
+clear_logger()
+keylogger_process = start_keylogger()
+time.sleep(2)
+
+num_of_lines = get_logger_lines_count()
+while True:
+    current_lines = get_logger_lines_count()
+    if num_of_lines != current_lines:
+      print get_logger_last_line()
+      num_of_lines = current_lines
